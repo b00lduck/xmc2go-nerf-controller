ae921639d6f2848dd20e10478404c24d7bff7619
Daniel Zerlett <daniel@zerlett.eu>
2016-12-10 14:23:19 +0100

    dual speed pwm

--- Makefile
+++ Makefile
@@ -21,7 +21,7 @@ DEVICE=-DXMC1100_Q024x0064
 
 CCFLAGS=-mcpu=cortex-m0 -mthumb -g --specs=nosys.specs -nostartfiles -T$(CMSIS)/Infineon/XMC1100_series/Source/GCC/XMC1100x0064.ld 
 	
-SRCS = main.c pwm.c adc.c stubs.c
+SRCS = main.c pwm.c adc.c stubs.c rpm.c
 SRCS += $(CMSIS)/Infineon/XMC1100_series/Source/GCC/startup_XMC1100.S
 SRCS += $(CMSIS)/Infineon/XMC1100_series/Source/system_XMC1100.c
 SRCS += $(XMCLIB)/src/xmc_gpio.c
--- main.c
+++ main.c
@@ -16,17 +16,6 @@ float desired_value = 0;
 
 int stalled = 0;
 
-#define SYSTICK_SHIFTER 16
-#define SYSTICKS_PER_SECOND (1 << SYSTICK_SHIFTER)
-#define SYSTICKS_PER_MINUTE (SYSTICKS_PER_SECOND * 60.0)
-
-float motor1_rpm = 0;
-float motor2_rpm = 0;
-
-uint32_t motor1_rotation_time = 0;
-uint32_t motor2_rotation_time = 0;
-
-
 int main(void) {
   XMC_GPIO_SetMode(LED1, XMC_GPIO_MODE_OUTPUT_PUSH_PULL);
   XMC_GPIO_SetMode(LED2, XMC_GPIO_MODE_OUTPUT_PUSH_PULL);
@@ -67,39 +56,7 @@ int main(void) {
 
   initAdc();
 
-  // init interrupt
-  XMC_ERU_ETL_CONFIG_t eruInit1 = {
-		  .input_a = MOTOR1_TACHOMETER_ERU_INPUT,
-		  .edge_detection = XMC_ERU_ETL_EDGE_DETECTION_FALLING,
-		  .enable_output_trigger = true,
-		  .output_trigger_channel = XMC_ERU_ETL_OUTPUT_TRIGGER_CHANNEL0,
-		  .source = XMC_ERU_ETL_SOURCE_A
-  };
-
-  XMC_ERU_ETL_CONFIG_t eruInit2 = {
-		  .input_a = MOTOR2_TACHOMETER_ERU_INPUT,
-		  .edge_detection = XMC_ERU_ETL_EDGE_DETECTION_FALLING,
-		  .enable_output_trigger = true,
-		  .output_trigger_channel = XMC_ERU_ETL_OUTPUT_TRIGGER_CHANNEL1,
-		  .source = XMC_ERU_ETL_SOURCE_B
-  };
-
-
-  XMC_ERU_OGU_CONFIG_t oguInit = {
-		  .service_request = true
-  };
-
-  XMC_ERU_ETL_Init(MOTOR1_TACHOMETER_ERU_ETL, &eruInit1);
-  XMC_ERU_OGU_Init(MOTOR1_TACHOMETER_ERU_OGU, &oguInit);
-
-  XMC_ERU_ETL_Init(MOTOR2_TACHOMETER_ERU_ETL, &eruInit2);
-  XMC_ERU_OGU_Init(MOTOR2_TACHOMETER_ERU_OGU, &oguInit);
-
-  NVIC_SetPriority(MOTOR1_TACHOMETER_ERU_IRQ, 15);
-  NVIC_EnableIRQ(MOTOR1_TACHOMETER_ERU_IRQ);
-
-  NVIC_SetPriority(MOTOR2_TACHOMETER_ERU_IRQ, 13);
-  NVIC_EnableIRQ(MOTOR2_TACHOMETER_ERU_IRQ);
+  initRpm();
 
 
   uint8_t trigger_running = 0;
@@ -111,25 +68,7 @@ int main(void) {
 	float foo = (float)adcGetPotBlocking() / 1024.0;
 	desired_value = foo * 23000.0;
 
-
-
-   	if (desired_value < 500) {
-	  stalled = 0;
-	  disableMotor1();
-	  disableMotor2();
-    } else if (((motor1_rotation_time >= SYSTICKS_PER_SECOND) ||
-    	  (motor2_rotation_time >= SYSTICKS_PER_SECOND)) &&
-		(desired_value > 1500)) {
-      stall();
-	}
-
-  	if (!stalled) {
-		float output1 = 0.0001 * (desired_value - motor1_rpm);
-		enableMotor1(output1);
-		float output2 = 0.0001 * (desired_value - motor2_rpm);
-		enableMotor2(output2);
-  	}
-
+	regulateRotors(desiredValue);
 
 	  //rotors = XMC_GPIO_GetInput(BUTTON1);
 	  //if (rotors != 1) {
@@ -169,30 +108,9 @@ int main(void) {
 
 }
 
-void ERU0_0_IRQHandler(void) {
-	motor1_rpm = SYSTICKS_PER_MINUTE / (float)motor1_rotation_time;
-	motor1_rotation_time = 0;
-	XMC_GPIO_ToggleOutput(LED1);
-}
 
-void ERU0_1_IRQHandler(void) {
-	motor2_rpm = SYSTICKS_PER_MINUTE / (float)motor2_rotation_time;
-	motor2_rotation_time = 0;
-	XMC_GPIO_ToggleOutput(LED2);
-}
-
-void stall() {
-	motor1_rotation_time = 0;
-	motor1_rpm = 0;
-	motor2_rotation_time = 0;
-	motor2_rpm = 0;
-	stalled = 1;
-	disableMotor1();
-	disableMotor2();
-}
 
 void SysTick_Handler(void) {
-	motor1_rotation_time ++;
-	motor2_rotation_time ++;
+	advanceRpmCounter();
 }
 
